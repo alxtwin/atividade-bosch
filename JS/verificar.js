4ce627ebf3b6e904f550383dc74dee27ad54f615
slvdrmln <alexlhmolina@gmail.com>
2021-11-21 10:23:55 -0300

    verifica conta criada

---
+++ JS/verificar.js
@@ -0,0 +1,15 @@
+//verifica o usuário e senha
+function verificar(){
+    var storedName = localStorage.getItem('name');
+    var storedPw = localStorage.getItem('pw');
+
+    var userName = document.getElementById('userName');
+    var userPw = document.getElementById('userPw');
+
+    if(userName.value == storedName && userPw.value == storedPw){
+        alert('Bem vindo '+userName.value+"!");
+        window.location=""
+    }else{
+        alert('Erro no login, verifique usuário e senha.');
+    }
+}
