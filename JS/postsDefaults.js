c52c5ea9c5e5628b1bc35b74ca98393dd2b3a159
slvdrmln <alexlhmolina@gmail.com>
2021-11-21 23:28:46 -0300

    feed populado, corrigido bug na foto do usuário no feed

--- JS/postsDefaults.js
+++ JS/postsDefaults.js
@@ -1,4 +1,48 @@
 var postsDefaults = [
+
+    {
+        imgUser: '../img/nerd.jpg',
+        nameUser: 'Frontenderson da Silva',
+        timePost: '20min',
+        qtdCurtidas: '850',
+        qtdComentarios: '500',
+        textPost: 'JavaScript que me deu!!',
+        qtdCompartilhamentos: '854',
+        imgPost: '../img/carro.jpg',
+    },
+
+    {
+        imgUser: '../img/programadora.jpg',
+        nameUser: 'Futura Programadora',
+        timePost: '2h',
+        qtdCurtidas: '60',
+        qtdComentarios: '7',
+        qtdCompartilhamentos: '1',
+        textPost: 'Legal demais essa notícia! Partiu Bosch com aquela ajuda da PUCPR!',
+        imgPost: '../img/bosch.jpg',
+    },
+    {
+        imgUser: '../img/ney.jpg',
+        nameUser: 'Ney',
+        timePost: '5h',
+        qtdCurtidas: '25.000',
+        qtdComentarios: '3.000',
+        qtdCompartilhamentos: '15.000',
+        textPost: 'Vamo dale!!',
+        imgPost: '../img/pai_ta_on.jpg',
+    },
+
+    {
+        imgUser: '../img/casimiro.jpg',
+        nameUser: 'Rapaz Engraçado',
+        timePost: '2d',
+        qtdCurtidas: '850',
+        qtdComentarios: '500',
+        textPost: 'Meteu essa mesmo?',
+        qtdCompartilhamentos: '1300',
+        imgPost: '../img/vasco.jpg',
+    },
+
     {
         imgUser: '../img/not_foto_user.png',
         nameUser: 'Tiozão do Gole',
@@ -8,21 +52,15 @@ var postsDefaults = [
         qtdCompartilhamentos: '1',
         imgPost: '../img/um_pouco_mais.jpg',
     },
+
     {
-        imgUser: '../img/not_foto_user.png',
-        nameUser: 'Tiozão do Gole',
-        timePost: '5h',
+        imgUser: '../img/nerd.jpg',
+        nameUser: 'Frontenderson da Silva',
+        timePost: '1h',
         qtdCurtidas: '60',
         qtdComentarios: '7',
         qtdCompartilhamentos: '1',
-        imgPost: '../img/pai_ta_on.jpg',
-    },
-    {
-        imgUser: '../img/not_foto_user.png',
-        nameUser: 'Robertão Visinho',
-        timePost: '4d',
-        qtdCurtidas: '55',
-        qtdComentarios: '3',
-        qtdCompartilhamentos: '5',
+        textPost: 'Recomendo demais!! perdendo o preconceito com back-end.',
+        imgPost: '../img/javacurso.jpg',
     },
 ]
